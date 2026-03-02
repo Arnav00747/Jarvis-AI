5b22a0f67ef3f58c9ce2db9fdce8d8243ed70015
ARNAV KUMAR <155212504+Arnav00747@users.noreply.github.com>
2026-03-02 18:25:48 +0530

    Initial upload - Jarvis AI project

---
+++ musiclibrary.py
@@ -0,0 +1,8 @@
+music={
+    "first":"https://www.youtube.com/watch?v=K4DyBUG242c",
+    "funk":"https://www.youtube.com/watch?v=_QFuWQaJUYs",
+    "mix":"https://www.youtube.com/watch?v=tTEWYJfB5eU&list=RDtTEWYJfB5eU&start_radio=1&rv=_QFuWQaJUYs",
+   
+
+
+}
